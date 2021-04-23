9e90a0a7635c986f88458bcc003987aa2c598240
gamzeeent <80635990+gamzeeent@users.noreply.github.com>
2021-04-23 20:26:45 +0300

    yemek tarifi sitesi

---
+++ yemektarifisitesi/yemektarifisitesi/anasayfa.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/kullanici.Master" AutoEventWireup="true" CodeBehind="anasayfa.aspx.cs" Inherits="yemektarifisitesi.anasayfa" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+    &nbsp;</p>
+</asp:Content>
---
+++ yemektarifisitesi/yemektarifisitesi/sqlsinif.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace yemektarifisitesi
+{
+    public class sqlsinif
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-LEMQV2R;Initial Catalog=dbo_yemektarifi;Integrated Security=true");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
